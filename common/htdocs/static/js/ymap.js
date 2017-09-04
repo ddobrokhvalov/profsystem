0b493b15d3f82db41e028b9662ce8edac50ff965
Dmitry Dobrokhvalov <ddobrokhvalov@gmail.com>
2017-09-04 22:40:52 +0300

    kontakts

---
+++ common/htdocs/static/js/ymap.js
@@ -0,0 +1,20 @@
+       ymaps.ready(init);
+        var myMap, 
+            myPlacemark;
+
+        function init(){ 
+            myMap = new ymaps.Map("map", {
+                center: [56.219793, 34.346096],
+                zoom: 13,
+                controls: ["zoomControl"]
+            }); 
+            
+ 			var myPlacemark = new ymaps.Placemark([56.219793, 34.346096], {}, {
+		        iconLayout: 'default#image',
+		        iconImageHref: './img/ymaps-icon.png',
+		        iconImageSize: [70, 90],
+		        iconImageOffset: [-35, -90]
+		    });
+            
+            myMap.geoObjects.add(myPlacemark);
+        }
